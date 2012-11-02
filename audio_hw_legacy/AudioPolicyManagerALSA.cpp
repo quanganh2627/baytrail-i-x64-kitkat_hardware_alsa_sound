0d35102f7e6c364cb79e20f6be25f1a28e26d294
Vincent Becker <vincentx.becker@intel.com>
2012-11-02 13:11:10 +0100

    [PORT FROM MAIN][AUDIO] Reduce log level for enforced audible stream type
    BZ: 69563
    
    In getDeviceForStrategy, use verbose log level in case of the
    enforced audible strategy.
    
    Change-Id: I7b4c9db1a5d5c91abde7b6de22d9cc9ae17a5b4b
    Origin-Change-Id: Ic744f77425a352083601aee644a261b0259470d1
    Signed-off-by: Vincent Becker <vincentx.becker@intel.com>
    Reviewed-on: http://android.intel.com:8080/81270
    Reviewed-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Tested-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- audio_hw_legacy/AudioPolicyManagerALSA.cpp
+++ audio_hw_legacy/AudioPolicyManagerALSA.cpp
@@ -369,14 +369,17 @@ audio_devices_t AudioPolicyManagerALSA::getDeviceForStrategy(routing_strategy st
             }
             break;
         case STRATEGY_ENFORCED_AUDIBLE:
-                if ( isInCall() && !mStreams[AUDIO_STREAM_ENFORCED_AUDIBLE].mCanBeMuted
-                     && ((getForceUse(AudioSystem::FOR_COMMUNICATION) == AudioSystem::FORCE_BT_SCO)
-                         || (currentDevice == AudioSystem::DEVICE_OUT_EARPIECE)) ) {
+                // During call, if an enforced audible stream is played, output it on the
+                // speaker + current device used for speech
+                if ( isInCall() &&
+                     !mStreams[AUDIO_STREAM_ENFORCED_AUDIBLE].mCanBeMuted &&
+                     ((getForceUse(AudioSystem::FOR_COMMUNICATION) == AudioSystem::FORCE_BT_SCO) ||
+                      (currentDevice == AudioSystem::DEVICE_OUT_EARPIECE)) ) {
                         // Set 2 devices for ALSA module : so the mode will be set as normal
                         //  - the BT SCO will be unrouted for instance during a shutter sound
                         //  during CSV call.
                         //  - the earpiece is not handled by the base class on MEDIA fall through case.
-                        ALOGD("%s- current device(0x%x) and ihf are concerned by Enforced Audible strategy", __FUNCTION__, currentDevice);
+                        ALOGV("%s- Current device(0x%x) and speaker stream output for an enforced audible stream", __FUNCTION__, currentDevice);
                         device = (mAvailableOutputDevices & AudioSystem::DEVICE_OUT_SPEAKER) | currentDevice;
                 }
             break;
