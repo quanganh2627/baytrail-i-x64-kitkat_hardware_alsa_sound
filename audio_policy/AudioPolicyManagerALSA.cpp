2a1ff53f3b252f4669b6e86b85dedeaee22b44c5
jerome <jerome.anand@intel.com>
2012-12-05 12:45:01 +0530

    [DO NOT UPSTREAM][Background Music] Support audio playback parallely without mixing
    BZ: 63786
    
    If music playback happens over WIDI device and if an incoming
    call comes; the music playback is allowed to follow the
    media strategy without interfering with the call
    
    Change-Id: Iaa831265551cafa6756c7a609c9aecb67330ee45
    Signed-off-by: Charitardha <satya.charitardha.jayanti@intel.com>
    Reviewed-on: http://android.intel.com:8080/79058
    Reviewed-by: Anand, Jerome <jerome.anand@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Sikkandar D, Madar <madar.sikkandar.d@intel.com>
    Reviewed-by: Kandasamy, Muthukumar <muthukumar.kandasamy@intel.com>
    Reviewed-by: Benavoli, Patrick <patrick.benavoli@intel.com>
    Reviewed-by: Krishna, Samaga <samaga.krishna@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- audio_policy/AudioPolicyManagerALSA.cpp
+++ audio_policy/AudioPolicyManagerALSA.cpp
@@ -334,6 +334,7 @@ audio_devices_t AudioPolicyManagerALSA::getDeviceForStrategy(routing_strategy st
     uint32_t device = 0;
 
     device = baseClass::getDeviceForStrategy(strategy, fromCache);
+
     AudioOutputDescriptor *hwOutputDesc = mOutputs.valueFor(mPrimaryOutput);
     uint32_t currentDevice = (uint32_t)hwOutputDesc->device();
 
@@ -373,16 +374,17 @@ audio_devices_t AudioPolicyManagerALSA::getDeviceForStrategy(routing_strategy st
         case STRATEGY_MEDIA:
             // We want to supersede the default policy when we are in call, so that we can change the volume
             // settings for MEDIA streams when in call, on BT SCO, SPEAKER or EARPIECE.
-            if (isInCall()){
+            if (isInCall()) {
                 // Retrieve the device in use for the call
                 AudioOutputDescriptor *hwOutputDesc = mOutputs.valueFor(mPrimaryOutput);
                 uint32_t currentDevice = (uint32_t)hwOutputDesc->device();
-                if ((currentDevice == AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_CARKIT)
+                if (((currentDevice == AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_CARKIT)
                     || (currentDevice == AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_HEADSET)
                     || (currentDevice == AudioSystem::DEVICE_OUT_BLUETOOTH_SCO)
                     || (currentDevice == AudioSystem::DEVICE_OUT_SPEAKER)
-                    || (currentDevice == AudioSystem::DEVICE_OUT_EARPIECE)){
-                    ALOGV("%s- superseding STRATEGY_MEDIA policy while in call from device 0x%x to 0x%x", __FUNCTION__, device, currentDevice);
+                    || (currentDevice == AudioSystem::DEVICE_OUT_EARPIECE))
+                    && ((device != AUDIO_DEVICE_OUT_REMOTE_BGM_SINK))) {
+                    ALOGD("%s- superseding STRATEGY_MEDIA policy while in call from device 0x%x to 0x%x", __FUNCTION__, device, currentDevice);
                     device = currentDevice;
                 }
             }
