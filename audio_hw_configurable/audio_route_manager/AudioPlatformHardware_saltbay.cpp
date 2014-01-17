55e45dc8f022699bedddef58b759e60160c4d33d
charitardha <satya.charitardha.jayanti@intel.com>
2014-01-17 20:41:43 +0530

    Music volume turns louder suddenly while ringtone is coming.
    BZ: 161858
    
    This patch removes MODE_RINGTONE for deep buffer route so that
    volume of the played music buffer is same even when ring comes.
    
    Change-Id: I495518b32c2d6d855c6605d38889a59284ffa1d3
    Category: device enablement
    Domain: AUDIO_MEDIA-Audio Policy
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: charitardha <satya.charitardha.jayanti@intel.com>

--- audio_hw_configurable/audio_route_manager/AudioPlatformHardware_saltbay.cpp
+++ audio_hw_configurable/audio_route_manager/AudioPlatformHardware_saltbay.cpp
@@ -230,7 +230,7 @@ const CAudioPlatformHardware::s_route_t CAudioPlatformHardware::_astAudioRoutes[
         },
         {
             NOT_APPLICABLE,
-            (1 << AudioSystem::MODE_NORMAL) | (1 << AudioSystem::MODE_RINGTONE)
+            (1 << AudioSystem::MODE_NORMAL)
         },
         mediaCardName,
         {
